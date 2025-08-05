96d9a88097150176f4b19908bb68423ed1d86905
Angeloamenta <Angeloamenta7@gmail.com>
2025-08-05 18:42:23 +0200

    edit home and button

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom"
+import Kettlebell from "../components/Kettlebell"
+import {Github} from "lucide-react"
+
+
+
 const Homepage = ()=> {
     return (
         <>
-        <h1>Homepage</h1>
+        <div className="hero">
+            <Kettlebell></Kettlebell>
+            {/* <div className="flex justify-center pb-4">
+                <Link to="/login"><button className="btn btn-orange">Login</button></Link>
+            </div> */}
+            <div className="flex justify-center items-center pb-5">
+                    <button className="text-xl btn-white">Log-in</button>
+                </div>
+            <div className=" flex justify-center items-center">
+                <p className="flex justify-center items-center">Created by Angelo Amenta <a className="p-2 my-3" href="https://github.com/Angeloamenta"><Github color="white" /></a></p>
+            </div>
+        </div>
         </>
     )
 }
